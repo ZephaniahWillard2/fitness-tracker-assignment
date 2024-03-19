2337f1045f981f2b8bbc099829211c931c66059a
Zephaniah-Willard <zepiwillard@gmail.com>
2024-03-19 15:30:26 -0500

    adding functionality

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let excerciseLogs = [
+    {exType: "Arms", setPer: 3, repPer: 12},
+    {exType: "Back", setPer: 3, repPer: 10},
+    {exType: "Chest", setPer: 4, repPer: 8},
+    {exType: "Legs", setPer: 3, repPer: 10},
+    {exType: "Cardio", setPer: null, repPer: null}
+];
+
+//stretch goal here
+
